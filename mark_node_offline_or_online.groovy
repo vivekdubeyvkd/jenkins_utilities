c506dc4642825544b38dce736307b867a5037016
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-07-01 13:22:35 +0530

    Update and rename mark_node_offline_online.groovy to mark_node_offline_or_online.groovy

--- mark_node_offline_or_online.groovy
+++ mark_node_offline_or_online.groovy
@@ -6,7 +6,7 @@ import hudson.model.*
 jenkins = Hudson.instance
 
 @NonCPS
-def mark_offline_or_bring_node_back_online(nodeName, actionType){
+def mark_node_offline_or_online(nodeName, actionType){
     for (int i =0; i < jenkins.slaves.size(); i++) {
         computer = jenkins.slaves[i].computer
         if(computer.name == 'ind-vmscm06-07.corp.intuit.net'){
@@ -24,5 +24,5 @@ def mark_offline_or_bring_node_back_online(nodeName, actionType){
     }  
 }
 // use is shown below
-mark_offline_or_bring_node_back_online('testnode', 'offline')
-mark_offline_or_bring_node_back_online('testnode', 'online')
+mark_node_offline_or_online('testnode', 'offline')
+mark_node_offline_or_online('testnode', 'online')
