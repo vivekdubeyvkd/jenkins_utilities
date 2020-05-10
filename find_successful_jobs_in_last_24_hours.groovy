a1b52a74a9cce9402ab4d765ccb5e4225dac3abc
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2020-05-10 11:47:00 +0530

    Create find_successful_jobs_in_last_24_hours.groovy

---
+++ find_successful_jobs_in_last_24_hours.groovy
@@ -0,0 +1,26 @@
+import hudson.model.Job
+import hudson.model.Result
+import hudson.model.Run
+import java.util.Calendar
+import jenkins.model.Jenkins
+
+count = 0
+//24 hours in a day, 3600 seconds in 1 hour, 1000 milliseconds in 1 second
+long time_in_millis = 24*3600*1000
+Calendar rightNow = Calendar.getInstance()
+
+Jenkins.instance.getAllItems(Job.class).findAll { Job job ->
+    !job.isBuilding()
+}.collect { Job job ->
+    //find all matching items and return a list but if null then return an empty list
+    job.builds.findAll { Run run ->
+        job.lastBuild.result == Result.SUCCESS && ((rightNow.getTimeInMillis() - run.getStartTimeInMillis()) <= time_in_millis)
+    } ?: []
+}.sum().each{ job -> 
+  if(job.result.toString() == 'SUCCESS') {
+    println "${job} ${job.result}" 
+    count++
+  }
+}
+
+println("Number of successful jobs in 24 hours : " + count)
