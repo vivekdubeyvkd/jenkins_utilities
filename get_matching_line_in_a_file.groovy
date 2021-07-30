be7f5c7678f0ba25a06b7c9f89858f96593a2125
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2021-07-30 11:35:05 +0530

    Create get_matching_line_in_a_file.groovy

---
+++ get_matching_line_in_a_file.groovy
@@ -0,0 +1,19 @@
+
+def get_matching_lines_in_file(filePath, pattern){
+    lines = readFile(fileName).readLines()
+    result = lines.findAll { it.contains(pattern) }
+    if(result[0]){
+        return result[0].toString()
+    }
+    return 
+}
+
+// call this function with absolute filePath and pattern that you want to search in that file
+// e.g. if vivek.txt in my file in current dir and vivek is pattern that I want to search, then I will call this function as shown below
+matchingLine = get_matching_lines_in_file("/tmp/vivek.txt", "vivek")
+if(matchingLine){
+   println("matching line found ") 
+   println(matchingLine) 
+} else {
+   println("No matching line found ") 
+}  
